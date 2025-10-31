261b1d8f06766c251f538a83831e104b17b8c627
flexGURU <mukunajohn329@gmail.com>
2025-10-31 17:53:43 +0300

    refactor: clean up Events.vue template and improve error message styling

--- frontend/src/pages/Events.vue
+++ frontend/src/pages/Events.vue
@@ -2,39 +2,34 @@
 	<div class="p-5">
 		<header class="flex justify-between items-center mt-5 md:max-w-7xl md:mx-auto">
 			<h1 class="text-3xl font-bold">Events</h1>
-			<!-- <Button variant="solid" theme="red" @click="toggleEventViews">
-        {{ toggleEventView ? "List" : "Calendar" }} View</Button
-      > -->
 		</header>
 	</div>
 	<div class="md:max-w- md:mx-auto">
+		<div class="flex justify-center mb-2 px-2">
+			<TextInput
+				type="search"
+				size="lg"
+				variant="outline"
+				placeholder="Search by event name or location"
+				:disabled="false"
+				:modelValue="searchTerm"
+				@update:modelValue="(val) => (searchTerm = val)"
+				class="w-full md:w-1/3"
+			/>
+		</div>
 		<ProgressSpinner v-if="events.loading" />
 		<ErrorMessage
-			v-if="events.error"
-			class="text-center border rounded-md p-2 border-red-500 bg-red-50 text-sm my-auto mt-20"
+			v-else-if="events.error"
+			class="text-center border rounded-md p-2 border-red-500 bg-red-50 text-sm my-auto mt-20 w-3/4 mx-auto"
 			message="Failed to load Events"
 		/>
-		<div>
-			<div class="flex justify-center mb-2 px-2">
-				<TextInput
-					type="search"
-					size="lg"
-					variant="outline"
-					placeholder="Search by event name or location"
-					:disabled="false"
-					:modelValue="searchTerm"
-					@update:modelValue="(val) => (searchTerm = val)"
-					class="w-full md:w-1/3"
-				/>
-			</div>
 
-			<EventCard
-				v-if="events.data && events.data.length > 0"
-				v-for="event in events.data"
-				:event="event"
-			/>
-			<EmptyState v-if="!events.data || events.data.length === 0" type="Events" />
+		<div v-else-if="events.data && events.data.length > 0">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-3/4 mx-auto gap-6 m-10">
+				<EventCard v-for="event in events.data" :event="event" />
+			</div>
 		</div>
+		<EmptyState v-else="!events.data || events.data.length === 0" type="Events" />
 	</div>
 	<div class="px-8">
 		<EventCalendar v-if="toggleEventView" :event="events.data" />
