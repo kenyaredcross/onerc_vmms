5cdb4a7afa9c0234860b866dc8e3cdb708c0b428
flexGURU <mukunajohn329@gmail.com>
2025-11-12 16:24:48 +0300

    feat(Sidebar): add sideBarApps utility for sidebar navigation

---
+++ frontend/src/utils/appsNavigate.js
@@ -0,0 +1,30 @@
+function sideBarApps() {
+	return [
+		{
+			name: "learning",
+			icon: "School",
+			title: __("Learning"),
+			route: "/lms",
+		},
+		{
+			name: "frappe",
+			icon: "Monitor",
+			title: __("Desk"),
+			route: "/app",
+		},
+		{
+			name: "forum",
+			icon: "MessageSquare",
+			title: __("Forum"),
+			route: "/raven",
+		},
+		{
+			name: "helpdesk",
+			icon: "HelpCircle",
+			title: __("Support"),
+			route: "/helpdesk/my-tickets",
+		},
+	];
+}
+
+export { sideBarApps };
