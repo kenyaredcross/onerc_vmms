5183ecf6df42ea1486ff57d7a9bd1601176eb789
flexGURU <mukunajohn329@gmail.com>
2025-11-13 08:34:21 +0300

    fix(VM Membership Type): update age requirement fields and validation logic

--- onerc_vmms/volunteer_and_member_management/doctype/vm_membership_type/vm_membership_type.js
+++ onerc_vmms/volunteer_and_member_management/doctype/vm_membership_type/vm_membership_type.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, Kenya Red Cross Society and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("VM Membership Type", {
-// 	refresh(frm) {
+frappe.ui.form.on("VM Membership Type", {
+	refresh(frm) {},
 
-// 	},
-// });
+	validate(frm) {
+		if (frm.doc.age_requirement) {
+			if (frm.doc.lower_age_limit >= frm.doc.upper_age_limit) {
+				frappe.throw(__("Lower Age Limit must be less than Upper Age Limit"));
+			}
+		}
+	},
+});
