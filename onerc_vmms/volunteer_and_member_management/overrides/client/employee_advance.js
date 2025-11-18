b2b9fa1d52855a20e283882c5f70ba1643985ee4
muruthigitau <muruthigitau@gmail.com>
2025-11-18 11:54:16 +0300

    Feat: Add client-side logic for Employee Advance doctype to copy data from PDR

---
+++ onerc_vmms/volunteer_and_member_management/overrides/client/employee_advance.js
@@ -0,0 +1,27 @@
+frappe.ui.form.on("Employee Advance", {
+	refresh(frm) {
+		copy_from_pdr(frm);
+	},
+	personnel_deployment_request(frm) {
+		copy_from_pdr(frm);
+	},
+});
+
+async function copy_from_pdr(frm) {
+	if (!frm.doc.personnel_deployment_request) return;
+	const pdr = await frappe.db.get_doc(
+		"Personnel Deployment Request",
+		frm.doc.personnel_deployment_request
+	);
+	if (!pdr) return;
+	copy_matching_fields(pdr, frm);
+}
+
+function copy_matching_fields(source_doc, frm) {
+	const target_fields = frm.fields_dict;
+	for (let field in source_doc) {
+		if (target_fields[field] && frm.doc[field] !== source_doc[field]) {
+			frm.set_value(field, source_doc[field]);
+		}
+	}
+}
