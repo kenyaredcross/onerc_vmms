5e27c9e77d2ff4a46918395ee9ef76e5a54e9f59
flexGURU <mukunajohn329@gmail.com>
2025-11-22 12:49:32 +0300

    refactor(attendee-booking): streamline attendee and ticket details management in session storage

--- frontend/src/utils/booking.ts
+++ frontend/src/utils/booking.ts
@@ -1,35 +1,77 @@
 import { ref } from "vue";
 
-const _attendeesState = ref<Attendee[]>([]);
-
+interface TicketDetails {
+  ticket_type: string;
+  number_of_tickets: number;
+  total_price: number;
+}
 interface Attendee {
   full_name: string;
   email: string;
   phone: string;
 }
+
+const _bookingDetails = ref<BookingData>({} as BookingData);
+
+interface BookingData {
+  ticketDetails: TicketDetails;
+  attendees: Attendee[];
+}
 export class AttendeeBooking {
-  private SESSION_KEY = "attendee_booking";
+  private ATTENDEE_SESSION_KEY = "attendee_booking";
+  private TICKET_SESSION_KEY = "ticket_details";
 
   constructor() {
-    this.loadFromSession();
+    this.bookingData();
   }
 
-  private loadFromSession() {
-    const data = sessionStorage.getItem(this.SESSION_KEY);
-    _attendeesState.value = data ? JSON.parse(data) : [];
+  set attendees(attendees: Attendee[]) {
+    sessionStorage.setItem(
+      this.ATTENDEE_SESSION_KEY,
+      JSON.stringify(attendees),
+    );
+    this.bookingData();
   }
-
-  saveAttendees(attendees: Attendee[]) {
-    sessionStorage.setItem(this.SESSION_KEY, JSON.stringify(attendees));
-    this.loadFromSession();
-  }
-  getAttendees(): Attendee[] {
-    return _attendeesState.value;
+  get attendees(): Attendee[] {
+    return sessionStorage.getItem(this.ATTENDEE_SESSION_KEY)
+      ? JSON.parse(sessionStorage.getItem(this.ATTENDEE_SESSION_KEY)!)
+      : [];
   }
 
   clearAttendees() {
-    sessionStorage.removeItem(this.SESSION_KEY);
-    this.loadFromSession();
+    sessionStorage.removeItem(this.ATTENDEE_SESSION_KEY);
+  }
+
+  set TicketDetails(details: TicketDetails) {
+    sessionStorage.setItem(this.TICKET_SESSION_KEY, JSON.stringify(details));
+    this.bookingData();
+  }
+
+  get TicketDetails(): TicketDetails {
+    const data = sessionStorage.getItem(this.TICKET_SESSION_KEY) ?? "";
+    return data ? JSON.parse(data) : ({} as TicketDetails);
+  }
+
+  clearTicketDetails() {
+    sessionStorage.removeItem(this.TICKET_SESSION_KEY);
+  }
+
+  clearBookingData() {
+    this.clearAttendees();
+    this.clearTicketDetails();
+    _bookingDetails.value = {} as BookingData;
+  }
+
+  private bookingData() {
+    const bookingData: BookingData = {
+      ticketDetails: this.TicketDetails,
+      attendees: this.attendees,
+    };
+    _bookingDetails.value = bookingData ? bookingData : ({} as BookingData);
+  }
+
+  getBookingData(): BookingData {
+    return _bookingDetails.value;
   }
 }
 export const attendeeBooking = new AttendeeBooking();
