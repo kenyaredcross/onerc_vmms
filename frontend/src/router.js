9e1f0d0385569094eed85137a1162619cced41b7
flexGURU <mukunajohn329@gmail.com>
2026-07-21 23:08:48 +0300

    fix broken events page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -110,6 +110,17 @@ const routes = [
 		component: () => import("@/pages/EventDetail.vue"),
 		props: true,
 	},
+	{
+		name: "EventRegistration",
+		path: "/event/registration/:eventRoute",
+		component: () => import("@/pages/EventRegistration.vue"),
+		props: true,
+	},
+	{
+		name: "CheckoutSummary",
+		path: "/checkout-summary",
+		component: () => import("@/pages/CheckoutSummary.vue"),
+	},
 ];
 
 let router = createRouter({
